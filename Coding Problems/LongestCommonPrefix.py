4d3967ad178ae0084c1a89e8d365e29e6fed2529
Joshua Neely <54088141+joshuaneelyCS@users.noreply.github.com>
2024-07-08 17:56:53 -0600

    Update LongestCommonPrefix.py

--- Coding Problems/LongestCommonPrefix.py
+++ Coding Problems/LongestCommonPrefix.py
@@ -1,16 +1,25 @@
 # Given a list of strings, find the longest shared prefix. If none, return ""
 
 def longestCommonPrefix(self, strs):
-        while True:
-            result = ""
-            if len(strs) == 1:
-                return strs[0]
-            for i in range (0,len(strs[0])):
-                letter = strs[0][i]
-                for word in strs:
-                    if i == len(word):
-                        return result
-                    if word[i] != letter:
-                        return result
-                result += letter
-            return result
+        if not strs:
+            return ""
+
+        def longestCommonPrefixRec(strs, left, right):
+            if left == right:
+                return strs[left]
+            else:
+                mid = (left + right) / 2
+                l = longestCommonPrefixRec(strs, left, mid)
+                r = longestCommonPrefixRec(strs, mid+1, right)
+                return commonPrefix(l,r)
+
+            return
+
+        def commonPrefix(left, right):
+            min_len = min(len(left), len(right))
+            for i in range(min_len):
+                if left[i] != right[i]:
+                    return left[:i]
+            return left[:min_len]
+
+        return longestCommonPrefixRec(strs, 0, len(strs)-1)
